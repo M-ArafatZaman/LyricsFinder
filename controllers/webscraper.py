6829aa7d23e4545b4746479f03be2a228e36e1ba
Mohammad Arafat Zaman <mdarafatzaman1@gmail.com>
2022-03-13 21:12:36 +0600

    Updated webscrapper to correctly format lyrics (Final)

--- controllers/webscraper.py
+++ controllers/webscraper.py
@@ -5,17 +5,20 @@ def scrapeLyricsFromURL(url):
     '''
     This function scrapes the genius url for lyrics and returns it.
     '''
+    lyrics: str = ""
     
     response = requests.get(url)
 
     # Convert response to bs4 object
     bs4Object = bs4.BeautifulSoup(response.content, "html.parser")
-    
-    # Lyrics div
-    divLyrics = bs4Object.find("div", id="lyrics-root")
-    adjustLines = str(divLyrics).replace("\n", "").replace("<br/>", "\n")
-    finalLyrics = bs4.BeautifulSoup(adjustLines, "html.parser")
-    
-    return finalLyrics.get_text()
+
+    for each in bs4Object.find_all("div", {"data-lyrics-container": "true"}):
+        adjustLines = str(each).replace("\n", "").replace("<br/>", "\n")
+        finalLyrics = bs4.BeautifulSoup(adjustLines, "html.parser")
+        lyrics += finalLyrics.get_text()
+        lyrics += "\n"
+
+    return lyrics
+
 
 
