7ad4a3ef5bac3e22c3e9976e8974445c83317fe2
Mohammad Arafat Zaman <mdarafatzaman1@gmail.com>
2022-09-10 06:17:43 +0600

    Added browser like user agent to webscraper.py

--- controllers/webscraper.py
+++ controllers/webscraper.py
@@ -11,8 +11,12 @@ def scrapeLyricsFromURL(url, cache=True):
     params = {}
     # Add a timestamp if cache is false
     if not cache: params["time"] = str(time.time())
+    # Browser like header
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    }
     
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=params, headers=headers)
 
     # Convert response to bs4 object
     bs4Object = bs4.BeautifulSoup(response.content, "html.parser")
